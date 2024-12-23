78be1a26f4047190f8ee36a560d89571494ccba3
Rupesh Bordiya <rbordiya22@hotmail.com>
2024-12-23 12:46:59 +0000

    day 16 -- setup

---
+++ aoc_python/day16/part2.py
@@ -0,0 +1,13 @@
+
+def solve_puzzle_p2(filename):
+    with open(filename, "r") as f:
+        grid_str, moves = f.read().split("\n\n")
+    pass
+
+
+
+if __name__ == "__main__":
+    filename = "example.txt"
+    print(f'Solution for example file is : {solve_puzzle_p2(filename)}')
+    filename = "day16_input.txt"
+    print(f'Solution for my file is : {solve_puzzle_p2(filename)}')
